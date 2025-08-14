a0eb7ed40f35be04ab8b928fb91cc509c9c2000b
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-08-14 14:58:58 -0400

    Time: 3 ms (12.19%), Space: 9.1 MB (22.96%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.cpp
@@ -6,10 +6,6 @@ public:
         int n=nums.size();
         int size=1;
         for(int i=1;i<nums.size();i++){
-              if(size==3){
-                ans+=nums[i-1];
-                max_ans=max(max_ans,ans);
-            }
             if(nums[i]==nums[i-1]){
                 size++;
                 ans+=nums[i-1];
@@ -18,12 +14,16 @@ public:
                 size=1;
                 ans="";
             }
+            if(size==3){
+                ans+=nums[i-1];
+                max_ans=max(max_ans,ans);
+            }
           
         }
-        if(size==3&&nums[n-3]==nums[n-2]&&nums[n-2]==nums[n-1]){
-            ans+=nums[n-1];
-            max_ans=max(max_ans,ans);
-        }
+        // if(size==3&&nums[n-3]==nums[n-2]&&nums[n-2]==nums[n-1]){
+        //     ans+=nums[n-1];
+        //     max_ans=max(max_ans,ans);
+        // }
         return max_ans;
     }
 };
