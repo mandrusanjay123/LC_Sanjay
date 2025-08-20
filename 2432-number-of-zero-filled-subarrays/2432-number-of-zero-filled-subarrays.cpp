f90a853abc98816c8d3fda591b2b31980a9cbf39
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-08-19 20:51:25 -0400

    Time: 3 ms (39.7%), Space: 116.3 MB (5.02%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     long long zeroFilledSubarray(vector<int>& nums) {
-        long long cnt = 0, streak = 0;
-        for (int num : nums) {
-            if (num == 0) {
-                streak++;
-                cnt += streak;
-            } else {
-                streak = 0;
+        int n = (int)nums.size();
+        if (n == 0) return 0;
+
+        std::vector<int> freq(n + 1, 0);
+
+        // Find frequencies of zero-run lengths
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0 && (i == 0 || nums[i - 1] != 0)) {
+                int cnt = 0;
+                int j = i;
+                while (j < n && nums[j] == 0) {
+                    ++cnt;
+                    ++j;
+                }
+                ++freq[cnt];
+                i = j - 1; // for-loop will ++i next
             }
         }
-        return cnt;
+
+        // Sum up number of subarrays from each zero-run length
+        long long ans = 0;
+        for (int len = 1; len <= n; ++len) {
+            if (freq[len] > 0) {
+                long long add = 1LL * len * (len + 1) / 2; // subarrays from one run
+                ans += add * freq[len];
+            }
+        }
+        return ans;
     }
 };
