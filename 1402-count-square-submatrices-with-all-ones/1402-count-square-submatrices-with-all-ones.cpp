ed289d3a2184701d5b5de77dea2a3706ea3edd99
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-08-21 10:27:40 -0400

    Time: 173 ms (5.01%), Space: 30.4 MB (21.42%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int total = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 1) {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1;  // first row/col
-                    } else {
-                        dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                    }
-                    total += dp[i][j];
-                }
+    int countSquares(vector<vector<int>>& mat) {
+        vector<vector<int>>hist(mat.size(),(vector<int>(mat[0].size(),0)));
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+               if(mat[i][j]==1&&i!=0){
+                hist[i][j]=mat[i][j]+hist[i-1][j];
+               }
+               else
+               hist[i][j]=mat[i][j];
             }
         }
-        return total;
+        int ans=0;
+         for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[i].size();j++){
+                cout<<hist[i][j]<<",";
+                int minH=INT_MAX;
+                for(int k=j;k>=0;k--){
+                    if(hist[i][k]==0)break;
+                    int width=j-k+1;
+                    minH=min(minH,hist[i][k]);
+                    if(minH>=width)ans+=1;
+                }
+
+            }
+            cout<<endl;
+            }
+            return ans;
     }
 };
