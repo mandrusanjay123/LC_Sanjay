275e480f04078730bbf43304fbdd00bb038dd434
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-08-22 18:47:26 -0400

    Time: 196 ms (97.89%), Space: 131.9 MB (98.49%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -19,9 +19,9 @@ public:
 
 
 
-// auto init = []() {
-//     ios::sync_with_stdio(0);
-//     cin.tie(0);
-//     cout.tie(0);
-//     return 'c';
-// }();
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
