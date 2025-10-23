dd276767aed5738f692da1827513a1719d59886f
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-10-22 23:33:14 -0400

    Time: 3 ms (87.54%), Space: 8.3 MB (95.79%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        if(s.length()==2){
-            if(s[0]==s[1])return true;
-            else return false;
+        int iteration = 0; // Tracks how many layers of reduction have been done
+
+        // Continue reducing until only two digits remain
+        while (s.size() - iteration != 2) {
+            // Replace each character with the sum of adjacent digits (mod 10)
+            for (int i = 0; i < s.size() - 1 - iteration; i++) {
+                s[i] = ((s[i] - '0') + (s[i + 1] - '0')) % 10 + '0';
+            }
+            iteration++;
         }
-        string temp="";
-        for(int i=1;i<s.length();i++){
-            temp+=((int)(s[i]-'0')+(int)(s[i-1]-'0'))%10;
-        }
-        return hasSameDigits(temp);
+
+        // Return true if the final two digits are equal
+        return s[0] == s[1];
     }
 };
