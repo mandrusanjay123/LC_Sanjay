99ddeab0420c1c478e7285d32277aeac9d49a00f
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-10-25 12:52:08 -0400

    Time: 0 ms (100%), Space: 7.9 MB (13.49%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -5,10 +5,10 @@ public:
         for(int i=1;i<=n;i++){
             if(i%7==0){
                 total_money+=7+i/7-1;
-                 cout<<7<<"+"<<i/7-1<<endl;
+                //  cout<<7<<"+"<<i/7-1<<endl;
             }
             else{
-            cout<<i%7<<"+"<<i/7<<endl;
+            // cout<<i%7<<"+"<<i/7<<endl;
             total_money+=i%7+i/7;
             }
         }
