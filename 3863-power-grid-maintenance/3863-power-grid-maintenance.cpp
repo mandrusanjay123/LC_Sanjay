519430bd7796f19bcd41beb5707ce212a658551f
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-07 23:03:40 -0500

    Time: 183 ms (73.33%), Space: 348.4 MB (71.86%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
@@ -1,50 +1,64 @@
-class UnionFind { // usual UnionFind class
-    vector<int> root, rank;
-public:
-    UnionFind(int N) : root(N+1), rank(N+1, 1){// for 1-indexed 
-        iota(root.begin(), root.end(), 0);
-    }
-
-    int Find(int x) {//Path compression
-        return (x == root[x])?x:root[x] = Find(root[x]);
-    }
-
-    bool Union(int x, int y) {//Union by rank
-        x= Find(x), y= Find(y);
-        if (x==y)
-            return 0;
-        if (rank[x] > rank[y])
-            swap(x, y);
-        root[x] = y;
-        if (rank[x] == rank[y])
-            rank[y]++;
-        return 1;
-    }
-};
-
+class DSU{   
+    vector<int>root;
+    vector<int>rank;
+    public:
+        DSU(int n):root(n+1),rank(n+1,-1){
+            iota(root.begin(),root.end(),0);
+        }
+        int find(int x){
+            if(x==root[x])return x;
+            return root[x]=find(root[x]);
+        }
+        bool Union(int a,int b){
+            a=find(a);
+            b=find(b);
+            if(a==b)return 0;
+            if(rank[a]>rank[b]){
+                root[b]=a;
+                rank[a]++;
+            }
+            else if(rank[a]<rank[b]){
+                root[a]=b;
+                rank[b]++;
+            }
+            else{
+                rank[a]++;
+                root[b]=a;
+            }
+            return 1;
+        }
+    };
 class Solution {
 public:
-    static vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
-        UnionFind G(c);
-        for(auto& e: connections){
-            G.Union(e[0], e[1]);
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        DSU g(c);
+        for(auto & c:connections){
+            g.Union(c[0],c[1]);
         }
-        vector<set<int>> comp(c+1);
-        for(int i=1; i<=c; i++){
-            comp[G.Find(i)].insert(i);
+        vector<set<int>>comp(c+1);
+        for(int i=1;i<=c;i++){
+            comp[g.find(i)].insert(i);
         }
-        vector<int> ans;
-        for(auto& q: queries){
-            const int t=q[0], x=q[1], rx=G.Find(x);
-            auto& C=comp[rx];
-            if (t==2)
+        vector<int>ans;
+        for(auto &q:queries){
+            int t=q[0];
+            int x=q[1];
+            int root_x=g.find(x);
+            auto &C=comp[root_x];
+            if(t==2){
                 C.erase(x);
-            else{
-                if (C.empty()) ans.push_back(-1);
-                else if (C.count(x)) ans.push_back(x);
-                else ans.push_back(*C.begin());
             }
-
+            else{
+                if(C.empty()){
+                    ans.push_back(-1);
+                }
+                else if(C.count(x)){
+                    ans.push_back(x);
+                }
+                else{
+                    ans.push_back(*C.begin());
+                }
+            }
         }
         return ans;
     }
