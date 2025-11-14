58a4d499588df5d2650ac594f79ecbd0257dd61a
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-14 12:41:15 -0500

    Time: 1650 ms (6.58%), Space: 86.3 MB (89.34%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -3,14 +3,11 @@ public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
         vector<vector<int>>ans(n,(vector<int>(n,0)));
         for(int i=0;i<queries.size();i++){
-            // for(int j=0;j<queries[i].size();j++){
                 for(int r=queries[i][0];r<=queries[i][2];r++){
                     for(int c=queries[i][1];c<=queries[i][3];c++){
-                        // cout<<r<<' '<<c<<endl;
                         ans[r][c]+=1;
                     }
                 }
-            // }
         }
         return ans;
     }
