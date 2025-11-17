2dd63dfe351addf6ca0b0bc9ae6fccd76de1bed8
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-17 13:47:22 -0500

    Time: 0 ms (100%), Space: 61.6 MB (13.45%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
         vector<int>ones_idxes;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==1)ones_idxes.push_back(i);
+        int lastOneIdx=0;
+        while(lastOneIdx<nums.size()&&nums[lastOneIdx]!=1){
+            lastOneIdx++;
         }
-        // for(int n:ones_idxes)cout<<n<<endl;
-        for(int i=1;i<ones_idxes.size();i++){
-            if(ones_idxes[i]-ones_idxes[i-1]-1<k)return false;
+        for(int i=lastOneIdx+1;i<nums.size();i++){
+           if(nums[i]==1){
+            if(i-lastOneIdx-1<k){
+                return false;
+            }
+            lastOneIdx=i;
+           }
         }
         return true;
     }
