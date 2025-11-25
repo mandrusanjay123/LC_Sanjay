e5873e453e3e6956d397157f32f2f7f03303174d
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-25 12:23:53 -0500

    Time: 0 ms (100%), Space: 7.9 MB (22.83%) - LeetHub

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k==1)return 1;
+        if(k%2==0||k%5==0)return -1;
+        int val=0;
+        for(int i=0;i<k;i++){
+            val=(val*10+1)%k;
+            if(val==0)return i+1;
+        }
+        return -1;
+    }
+};
