2f6b96a540526e5f111f8443adfb5b63fa595a99
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-27 20:02:51 -0500

    Time: 0 ms (100%), Space: 162.3 MB (77.52%) - LeetHub

--- 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -8,11 +8,11 @@ public:
         minPrefix[0] = 0;
         for (int i = 0; i < n; i++) {
             prefix += nums[i];
-            int mod = ((i + 1) % k);
-            if (minPrefix[mod] != LLONG_MAX) {
-                ans = max(ans, prefix - minPrefix[mod]);
+            int j = ((i + 1) % k);
+            if (minPrefix[j] != LLONG_MAX) {
+                ans = max(ans, prefix - minPrefix[j]);
             }
-            minPrefix[mod] = min(minPrefix[mod], prefix);
+            minPrefix[j] = min(minPrefix[j], prefix);
         }
         return ans;
     }
