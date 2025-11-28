be72d4a912d55079f181520546d107c87c30a27f
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-11-28 12:41:01 -0500

    Time: 154 ms (41.28%), Space: 185 MB (43.77%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int components = 0;
-
-    long long dfs(int u, int parent, vector<vector<int>>& adj, vector<int>& values, int k) {
-        long long sum = values[u];
-
-        for (int v : adj[u]) {
-            if (v == parent) continue;
-            sum += dfs(v, u, adj, values, k);
-        }
-
-        if (sum % k == 0) {
-            components++;
-            return 0;           // cut this component
-        }
-
-        return sum;             // pass sum upward
+int components=0;
+long long dfs(int u,int parent,vector<vector<int>>&adj,vector<int>&values,int k){
+    long long sum=values[u];
+    for(int neigh:adj[u]){
+        if(neigh==parent)continue;
+        // 
+        sum+=dfs(neigh,u,adj,values,k);
+        
     }
-
+    if(sum%k==0){
+        components++;
+        return 0;
+    }
+    else{
+        return sum;
+    }
+    return 0;
+}
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        vector<vector<int>> adj(n);
-
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        vector<vector<int>>adj(n);
+        for(auto edge:edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
-
-        dfs(0, -1, adj, values, k);
-
+        dfs(0,-1,adj,values,k);
         return components;
     }
 };
