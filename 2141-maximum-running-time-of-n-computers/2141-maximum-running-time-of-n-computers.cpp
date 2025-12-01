5e34c71cf1f11913c59b5c865846b427029ced1c
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-12-01 18:05:23 -0500

    Time: 24 ms (52.1%), Space: 59.7 MB (59.22%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
+    long long possible(vector<int>& batteries,long long mid,int n){
+      long long total=0;
+      for(int battery:batteries){
+        total+=min((long long)battery,mid);
+      }
+      return total>=mid*n;
+    }
     long long maxRunTime(int n, vector<int>& batteries) {
-        long long sum = 0;
-        for (long long b : batteries) sum += b;
-
-        long long low = 1, high = sum / n, ans = 0;
-
-        while (low <= high) {
-            long long mid = (low + high) / 2;
-
-            long long total = 0;
-            for (auto b : batteries)
-                total += min((long long)b, mid);
-
-            if (total >= mid * n) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        long long sum=0;
+        long long low=LLONG_MAX;
+        for(int charge:batteries)
+        {   low=min((long long)charge,low);
+            sum+=charge;
+        }
+        long long high=sum;
+        long long ans=0;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(possible(batteries,mid,n)){
+                low=mid+1;
+                ans=mid;
+            }
+            else{
+                high=mid-1;
             }
         }
         return ans;
