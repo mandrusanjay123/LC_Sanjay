2943a401dcb60575be71e6fa911d031a181d9e21
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-12-04 10:28:15 -0500

    Time: 14 ms (57.1%), Space: 19.4 MB (50.72%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int block=0;
+        int right=0;
+        int left=0;
+        int collisions=0;
+        for(int i=0;i<directions.size();i++){
+            if(directions[i]=='R'){
+                right++;
+            }
+            else if(directions[i]=='L'){
+                if(right>0){
+                    collisions+=(right+1);
+                    right=0;
+                    block++;
+                }
+                else if(block>0){
+                    collisions++;
+                    block++;
+                }
+            }
+            else{
+                block++;
+                collisions+=right;
+                right=0;
+            }
+        }
+        return collisions;
+    }
+};
