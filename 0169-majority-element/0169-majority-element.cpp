e82099e4e543de5d631a80189b0c0cc41c75487a
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-12-24 14:32:55 -0500

    Time: 0 ms (100%), Space: 28.1 MB (86.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element=0;
+        bool election=false;
+        int vote=0;
+        for(int i=0;i<nums.size();i++){
+            if(election&&nums[i]==element){
+                vote++;
+            }
+            if(!election){
+                element=nums[i];
+                election= true;
+                vote=1;
+            }
+            if(nums[i]!=element){
+                vote--;
+            }
+            if(vote==0){
+                election =false;
+            }
+        }
+        cout<<vote;
+        return element;
+    }
+};
