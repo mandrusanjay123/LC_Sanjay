36342c2fd00f4fee9c470a4b86a778b7fd872a66
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-12-24 19:08:21 -0500

    Time: 0 ms (100%), Space: 33 MB (92.82%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.cpp
@@ -3,14 +3,13 @@ public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
         sort(capacity.begin(),capacity.end(),greater<int>());
         int sum=0;
-        for(int i=0;i<apple.size();i++){
-            sum+=apple[i];
-        }
-        int i;
-        for(i=0;i<capacity.size();i++){
-            sum-=capacity[i];
+        for(int a:apple)sum+=a;
+        int id=0;
+        for(int c:capacity){
+            id++;
+            sum-=c;
             if(sum<=0)break;
         }
-        return i+1;
+        return id;
     }
 };
