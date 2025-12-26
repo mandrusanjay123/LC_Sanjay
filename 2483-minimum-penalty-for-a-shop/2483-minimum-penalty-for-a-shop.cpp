781204fb5020f579b89936e4acfe8cbf4d3c8dd6
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2025-12-26 13:21:34 -0500

    Time: 7 ms (67.76%), Space: 13.4 MB (67.65%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -6,14 +6,11 @@ public:
         for(int c:customers){
             if(c=='Y')activeCustomers++;
         }
-        //YNNNNNN
-        // if(activeCustomers==n)return n;
-        if(activeCustomers==0)return 0;
+        // if(activeCustomers==0)return 0;
         int minPenality=n;
         int currPenality=activeCustomers;
         int minId=INT_MAX;
         cout<<minPenality<<endl;
-        // cout<<0<<","<<currPenality<<endl;
         int id;
         for(id=0;id<n;id++){
               if(currPenality<minPenality){
@@ -26,8 +23,6 @@ public:
             else if(customers[id]=='N'){
                 currPenality++;
             }
-          
-            // cout<<id+1<<","<<currPenality<<endl;
         }
         if(currPenality<minPenality)minId=id;
         return minId;
