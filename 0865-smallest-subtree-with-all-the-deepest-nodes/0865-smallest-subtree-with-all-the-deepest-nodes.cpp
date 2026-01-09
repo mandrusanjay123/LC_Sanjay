e7c81e8a30b07a0a02413e049d4487e43050a02c
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-01-09 11:26:01 -0500

    Time: 0 ms (100%), Space: 16.3 MB (52.91%) - LeetHub

--- 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     pair<TreeNode*,int> maxDepth(TreeNode* node, int depth){
         if(node==NULL)return {NULL,depth};
-        if(node!=NULL){
+        // if(node!=NULL){
             pair<TreeNode*,int>nodeLeft=maxDepth(node->left,depth+1);
             pair<TreeNode*,int>nodeRight=maxDepth(node->right,depth+1);
             if(nodeLeft.second==nodeRight.second){
@@ -21,7 +21,7 @@ public:
             }
             if(nodeLeft.second>nodeRight.second)return {nodeLeft.first,nodeLeft.second};
             else if(nodeRight.second>nodeLeft.second)return {nodeRight.first,nodeRight.second};
-        }
+        // }
         return {NULL,depth};
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
