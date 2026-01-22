c11dd8580c1f7bbe5cfcb6371a8d5da23247e3a8
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-01-22 12:13:10 -0500

    Time: 67 ms (24.01%), Space: 61.7 MB (48.54%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+bool ispalindrome(string s,int l,int r){
+    // int l=0;
+    // int r=s.length()-1;
+    while(l<r){
+        if(s[l++]!=s[r--])return false;
+    }
+    return true;
+}
+void recursion(int id,int n,vector<string>&temp, vector<vector<string>>&ans,string s){
+    if(id==n){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=id;i<n;i++){
+        //0 to id -->substr1
+        //id=1 to n-1--?substr2
+
+        if(ispalindrome(s,id,i)){
+            temp.push_back(s.substr(id,i-id+1));
+            recursion(i+1,n,temp,ans,s);
+            temp.pop_back();
+        }
+    }
+    return;
+}
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>temp;
+        recursion(0,s.length(),temp,ans,s);
+        return ans;
+    }
+};
