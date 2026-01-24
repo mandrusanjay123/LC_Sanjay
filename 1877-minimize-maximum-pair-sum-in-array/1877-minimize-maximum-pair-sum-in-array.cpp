ee57a39b1abaca172806200a24fa5cd7e71727fd
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-01-24 11:08:24 -0500

    Time: 172 ms (56.92%), Space: 100.1 MB (30.18%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -4,14 +4,16 @@ public:
         sort(nums.begin(),nums.end());
         int l=0;
         int r=nums.size()-1;
-        vector<int>sums;
-        while(l<r){
-            sums.push_back(nums[l++]+nums[r--]);
-        }
+        // vector<int>sums;
         int ans=0;
-        for(int s:sums){
-            ans=max(ans,s);
+        while(l<r){
+            // sums.push_back(nums[l++]+nums[r--]);
+            ans=max(ans,nums[l++]+nums[r--]);
         }
+        
+        // for(int s:sums){
+        //     ans=max(ans,s);
+        // }
         return ans;
     }
 };
