ac5e6b528f3a459aa5aa6790337b4c5a8ca95f52
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-02-16 09:39:51 -0500

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int reverseBits(int n) {
         int ans=0;
-        // string bits="";
         int range=32;
         while(range>0){
             if(n&1){
-                // bits+='1';
                 ans=ans*2+1;
             }
             else{
-                // bits+='0';
                 ans=ans*2;
             }
             n>>=1;
             range--;
         }
-        // cout<<bits;
         return ans;
     }
 };
