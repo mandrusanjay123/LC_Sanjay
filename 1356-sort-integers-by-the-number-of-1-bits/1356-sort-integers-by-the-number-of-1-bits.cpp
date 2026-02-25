62c4606d9c959854de132079ef5bd7b296f71a04
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-02-25 09:46:04 -0500

    Time: 0 ms (100%), Space: 14.9 MB (18.95%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int>ans;
+        vector<pair<int,int>>store;
+        for(int i=0;i<arr.size();i++){
+            int num=arr[i];
+            int num_bits=0;
+            while(num>0){
+                num_bits+=(num&1)?1:0;
+                num>>=1;
+            }
+            store.push_back({num_bits,arr[i]});
+        }
+        sort(store.begin(),store.end(),[](const pair<int,int>&a,pair<int,int>&b){
+            if(a.first==b.first)return a.second<b.second;
+            return a.first<b.first;
+        });
+        for(int i=0;i<store.size();i++){
+            ans.push_back(store[i].second);
+        }
+        return ans;
+    }
+};
