2ba00ea084094a3385a7c9d968053601929ea73e
Sanjay Kumar Mandru <46988513+mandrusanjay123@users.noreply.github.com>
2026-03-18 10:45:07 -0400

    Time: 19 ms (40.45%), Space: 101 MB (50.56%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
         vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),0));
-
+        int count=0;
         for(int i = 0;i < grid.size(); i++){
 
             for(int j = 0; j<grid[i].size(); j++){
@@ -19,20 +19,21 @@ public:
                 if(previ >= 0 && prevj >= 0){
                     dp[i][j] -= dp[previ][prevj];
                 }
+                 if(dp[i][j]<=k) count++;
                 // cout<<dp[i][j]<<","; 
             }
             // cout<<endl;
 
         }
 
-        int count=0;
-            for(int i = 0; i<grid.size(); i++){
-                for(int j=0 ;j<grid[i].size(); j++){
+        // int count=0;
+        //     for(int i = 0; i<grid.size(); i++){
+        //         for(int j=0 ;j<grid[i].size(); j++){
                     
-                    if(dp[i][j]<=k) count++;
-                }
+        //             if(dp[i][j]<=k) count++;
+        //         }
                 
-            }
+        //     }
 
         return count;
     }
